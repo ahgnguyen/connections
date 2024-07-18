0da61498fbd24cd3e6b2c810bc93d56ed0297b04
Zhang <cjz@80a9972dc2a6.amazon.com>
2024-07-17 23:11:17 -0700

    Update categories and blurbs.

--- src/lib/data.js
+++ src/lib/data.js
@@ -4,27 +4,25 @@ export const CONNECTION_GAMES = [
       category: "Mascots",
       words: ["Husky", "Viking", "Matador", "Red"],
       difficulty: 1,
-      imageSrc: "https://i.ibb.co/Gn36Ypb/Connections-1.png",
+      imageSrc: "https://i.imgur.com/oNl86BJ.png",
     },
     {
-      category: "Words Starting With \"Le___\"",
+      category: "Athletes With Food Names",
+      words: ["Plum", "Curry", "Mayo", "Lamb"],
+      difficulty: 3,
+      imageSrc: "https://i.imgur.com/f0kozJT.png",
+    },
+    {
+      category: "Hot ___",
+      words: ["Pot", "Ones", "Pocket", "Swap"],
+      difficulty: 4,
+      imageSrc: "https://i.imgur.com/vjr3fyj.png",
+    },
+    {
+      category: "Words Starting With Le___",
       words: ["Sserafim", "Vain", "Bron", "Mon"],
       difficulty: 2,
-      imageSrc: "https://i.ibb.co/3B44zXX/Connections-2.png",
-    },
-    {
-      category: "Restaurants with Hot Names",
-      words: ["Dave's", "Wings", "Lunch", "Point"],
-      difficulty: 3,
-      imageSrc: "https://i.ibb.co/4MwD7zv/Connections-3.png",
-    },
-
-    {
-      category: "Suffix Homophones for Coding Words",
-      words: ["Shin", "Bass", "Rhythm", "Wear"],
-      difficulty: 4,
-      imageSrc:
-        "https://i.ibb.co/V2vnr5P/boys2men-2-d53afdddf480401fac1204a3954aa5f6.webp",
+      imageSrc: "https://i.imgur.com/SBHbLBh.png",
     },
   ]
 ];
